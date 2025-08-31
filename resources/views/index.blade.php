d60183a525f1f46857ac9fd906167777ed9be24a
Sap_tea Version_1.0 <saptea.projetofoccus@gmail.com>
2025-08-31 07:48:42 -0300

    Atualizações no arquivo index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@ xxxxxxxxxxxxxxxxxxxxxxxxxxxx
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    xxxxxxxxxxxxxxxxxxxxxxxx
+ 
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
